3edd2e7c93e9c104507e50d626fdb022bd82ac4a
fabiomigueldp <fabiomigueldp@gmail.com>
2023-07-03 06:29:29 -0300

    commitii

--- FMDP-Alg-05-Ex-16.py
+++ FMDP-Alg-05-Ex-16.py
@@ -1,3 +1,16 @@
+'''16. Conversão arbitrária de base numérica. Escreva um programa que permita ao usuário
+converter um número de uma base para base 10 e vice-versa. Seu programa deve suportar
+bases entre 2 (binário) e 16 (hexadecimal) para o número de entrada e o número de resultado.
+Divida seu programa em várias funções, incluindo uma função que converte de uma base
+arbitrária em uma base 10, uma função que converte de uma base 10 em uma base arbitrária.
+A primeira função deve receber como parâmetros uma string com o número a ser convertido
+para base 10, e o valor da base deste número (portanto, de 2 a 16) e deve retornar como
+resultado o número convertido na base 10. A segunda função deve receber como parâmetros
+um numero na base 10 e a base para qual queremos converter o número. Como resultado, a
+função deve retornar uma string com o número convertido. Além disso, faça um programa
+principal que lê as bases e o número de entrada do usuário. Você pode encontrar parte da
+solução deste problema no exercício anterior e nos exercícios 14 e 15 da lista 4.'''
+
 def convert_to_decimal(number, base):
     decimal = 0
     power = 0
@@ -28,4 +41,3 @@ def main():
     result = convert_from_decimal(decimal, base_output)
     print("O número convertido é:", result)
 
-main()
